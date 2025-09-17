cd25a370f927363ec56ab8e2963ad8fd2f09d047
jacobtakaoka <jaketaka@hotmail.com>
2025-09-16 19:14:25 -0700

    Add files via upload

---
+++ suit.dart
@@ -0,0 +1,33 @@
+import 'package:flame/sprite.dart';
+import 'package:flutter/foundation.dart';
+import 'klondike_game.dart';
+
+@immutable
+class Suit {
+  //Factory Constructor. Enforces singleton pattern.
+  factory Suit.fromInt(int index) {
+    assert(
+    index >= 0 && index <= 3,
+    );
+    return _singletons[index];
+  }
+
+  //Private constructor. Initializes properties of each Suit (number, label, sprite)
+  Suit._(this.value, this.label, double x, double y, double w, double h)
+      : sprite = klondikeSprite(x, y, w, h);
+
+  final int value;
+  final String label;
+  final Sprite sprite;
+
+  static final List<Suit> _singletons = [
+    Suit._(0, '♥', 1176, 17, 172, 183),
+    Suit._(1, '♦', 973, 14, 177, 182),
+    Suit._(2, '♣', 974, 226, 184, 172),
+    Suit._(3, '♠', 1178, 220, 176, 182),
+  ];
+
+  /// Hearts and Diamonds are red, while Clubs and Spades are black.
+  bool get isRed => value <= 1;
+  bool get isBlack => value >= 2;
+}
